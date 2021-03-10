6a2ad48815bd601c41bf7766d91b7bea076d1cc2
mikalai mazur <mazurnikv@gmail.com>
2021-03-10 17:27:01 +0300

    add js and finished app

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -33,3 +33,81 @@ const quizData = [
     },
 ];
 
+
+const quiz = document.getElementById('quiz');
+const qEl = document.getElementById ('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+const answerEls = document.querySelectorAll('.answer');
+
+
+let currentQuiz = 0;
+let score = 0;
+
+
+loadQuiz();
+
+
+function loadQuiz() {
+    deselectAnswer()
+
+    const currentQuizData = quizData[currentQuiz];
+
+    qEl.innerText = currentQuizData.question;
+    
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+}
+
+function getSelected() {
+
+    let answer = undefined;
+   
+
+    answerEls.forEach((answerEl)=>{
+        if(answerEl.checked){
+            answer =  answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswer(){
+    answerEls.forEach((answerEl)=>{
+        answerEl.checked = false;
+    });
+}
+
+
+
+
+submitBtn.addEventListener("click", () => {
+    
+    const answer = getSelected();
+    
+    if (answer){
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+            // console.log(score)
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+            
+        } else {
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions!</h2> <button onclick = 'location.reload()'>Reload</button>`;
+            
+        }
+    }
+
+    
+})
+
